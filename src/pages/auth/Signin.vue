d65cfe3a3fcebb6db19b528af5f6a00b3810a4f8
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-06-26 22:04:32 +0900

    6로그인화면

---
+++ src/pages/auth/Signin.vue
@@ -0,0 +1,18 @@
+<template>
+    <div align="center">
+        <h2>로그인</h2>
+        <signin-form/>
+    </div>
+</template>
+
+<script>
+import SigninForm from '@/components/auth/SigninForm'
+
+export default {
+    name: 'Signin',
+    components: {
+        SigninForm 
+    }
+
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-/*
-import HelloWorld from '@/components/HelloWorld'
-*/
 import { HomeRouters } from '@/router/home'
+import { AuthRouters } from '@/router/auth'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    /*
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-    */
-    ...HomeRouters
+    ...HomeRouters,
+    ...AuthRouters
   ]
 })
