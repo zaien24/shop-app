c45a0039a921390128fc362e0dd9d8be71cee7f5
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-06-26 22:27:13 +0900

    7   최초 관리자 생성

---
+++ src/pages/member/AdminSetupPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div align="center">
+    <h2>최초관리자 등록</h2>
+    <admin-setup-form @submit="onSubmit"/>
+  </div>
+</template>
+
+<script>
+import api from '@/api'
+import AdminSetupForm from '@/components/member/AdminSetupForm'
+
+export default {
+  name: 'AdminSetupPage',
+  components: { AdminSetupForm },
+  methods: {
+    onSubmit (payload) {
+      const { userId, userName, userPw } = payload
+      api.post('/users/setup', { userId, userName, userPw })
+        .then(res => {
+          alert('등록이 완료되었습니다.')
+          this.$router.push({
+            name: 'Home'
+          })
+        })
+        .catch(err => {
+          alert(err.response.data)
+        })
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { HomeRouters } from '@/router/home'
 import { AuthRouters } from '@/router/auth'
+import { MemberRouters } from '@/router/member'
 
 Vue.use(Router)
 
@@ -9,6 +10,7 @@ export default new Router({
   mode: 'history',
   routes: [
     ...HomeRouters,
-    ...AuthRouters
+    ...AuthRouters,
+    ...MemberRouters
   ]
 })
